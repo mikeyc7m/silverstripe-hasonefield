26100f57955da8878867b8fd31e03eaa02ccfb9b
Michael Caruana <mikeyc7m@users.noreply.github.com>
2022-05-02 10:40:47 +1000

    more tidy

---
+++ HasOneField.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * HasOneField is a tidy way to display & manipulate a has_one relationship. Includes Create, Edit, Unlink, Delete, and
+ * a search feature to Link to existing items.
+ *
+ * Example Usage: $field = new HasOneField($this, 'MyRelation', 'My Related Field');
+ *
+ * @package  michael.caruana/hasonefield
+ * @author  Michael Caruana <mikeyc7m@gmail.com>
+ */
+class HasOneField extends CompositeField
+{
+    public function __construct($parent, $relation, $title = '')
+    {
+        parent::__construct(GridFieldHasOneField::create($parent, $relation, $title));
+        $this->setName($relation . '_HasOneHolder')
+            ->setLegend(CompositeField::name_to_label($relation))
+            ->setTag('fieldset')
+            ->addExtraClass('gridfield-has-one');
+    }
+}
